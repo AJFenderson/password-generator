220d7e9310e18a626e936f45a7541edb95d1b26f
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-02-26 01:07:49 -0600

    added comments to js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -17,17 +18,18 @@ function generatePassword() {
   if (input >= 8 && input <= 128) {
     passwordLength = parseInt(input);
     
-     confirm("Would you like to include lowercase letters?");
+    confirm("Would you like to include lowercase letters?");
      confirm("Would you like to include capital letters?");
     confirm("Would you like to include numbers?");
     confirm("Would you like to include special characters?"); 
     
   } 
+  //if the user does not choose a number to meet requirement it will alert them//
   else if (input < 8 || input > 128) {
     
     alert ("Does not meet requirement.Please try again");
   }
-
+// the application will return back to the prompt to enter a number for the length of the password//
   return password;
 }
 
